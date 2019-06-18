openshift/origin

Add an OAuth2 server and authn abstractions

pkg/authn/authenticator/requestheader/requestheader.go
@@ -1,0 +1,36 @@
+package requestheader
+
+import (
+	"net/http"
+
+	"github.com/openshift/origin/pkg/authn/api"
+)
+
+type Config struct {
+	UserNameHeader string
+}
+
+func NewDefaultConfig() *Config {
+	return &Config{
+		UserNameHeader: "X-Remote-User",
+	}
+}
+
+type Authenticator struct {
+	config *Config
+}
+
+func NewAuthenticator(config *Config) *Authenticator {
+	return &Authenticator{config}
+}
+
+func (a *Authenticator) AuthenticateRequest(req *http.Request) (api.UserInfo, bool, error) {
+	name := req.Header.Get(a.config.UserNameHeader)
+	if name == "" {
+		return nil, false, nil
+	}
+	user := &api.DefaultUserInfo{
+		Name: name,
+	}
+	return user, true, nil
+}

pkg/authn/oauth/handlers/authenticator.go
@@ -1,0 +1,108 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/RangelReale/osin"
+	"github.com/golang/glog"
+
+	"github.com/openshift/origin/pkg/authn/api"
+	"github.com/openshift/origin/pkg/authn/authenticator"
+)
+
+type AuthorizeAuthenticator struct {
+	handler AuthenticationHandler
+	request authenticator.Request
+}
+
+func NewAuthorizeAuthenticator(handler AuthenticationHandler, request authenticator.Request) *AuthorizeAuthenticator {
+	return &AuthorizeAuthenticator{handler, request}
+}
+
+func (h *AuthorizeAuthenticator) HandleAuthorize(ar *osin.AuthorizeRequest, w http.ResponseWriter, req *http.Request) (handled bool) {
+	info, ok, err := h.request.AuthenticateRequest(req)
+	if err != nil {
+		h.handler.AuthenticationError(err, w, req)
+		return true
+	}
+	if !ok {
+		h.handler.AuthenticationNeeded(w, req)
+		return true
+	}
+	ar.UserData = info
+	ar.Authorized = true
+	return
+}
+
+type AccessAuthenticator struct {
+	password  authenticator.Password
+	assertion authenticator.Assertion
+	client    authenticator.Client
+}
+
+func NewAccessAuthenticator(password authenticator.Password, assertion authenticator.Assertion, client authenticator.Client) *AccessAuthenticator {
+	return &AccessAuthenticator{password, assertion, client}
+}
+
+func (h *AccessAuthenticator) HandleAccess(ar *osin.AccessRequest, w http.ResponseWriter, req *http.Request) {
+	switch ar.Type {
+	case osin.AUTHORIZATION_CODE, osin.REFRESH_TOKEN:
+		// auth codes and refresh tokens are assumed allowed
+	case osin.PASSWORD:
+		info, ok, err := h.password.AuthenticatePassword(ar.Username, ar.Password)
+		if err != nil {
+			glog.Errorf("Unable to authenticate password: %v", err)
+			return
+		}
+		if !ok {
+			return
+		}
+		ar.AccessData.UserData = info
+	case osin.ASSERTION:
+		info, ok, err := h.assertion.AuthenticateAssertion(ar.AssertionType, ar.Assertion)
+		if err != nil {
+			glog.Errorf("Unable to authenticate password: %v", err)
+			return
+		}
+		if !ok {
+			return
+		}
+		ar.AccessData.UserData = info
+	case osin.CLIENT_CREDENTIALS:
+		info, ok, err := h.client.AuthenticateClient(ar.Client)
+		if err != nil {
+			glog.Errorf("Unable to authenticate password: %v", err)
+			return
+		}
+		if !ok {
+			return
+		}
+		ar.AccessData.UserData = info
+		return
+	default:
+		glog.Warningf("Received unknown access token type: %s", ar.Type)
+		return
+	}
+
+	ar.Authorized = true
+}
+
+func NewDenyAccessAuthenticator() *AccessAuthenticator {
+	return &AccessAuthenticator{Deny, Deny, Deny}
+}
+
+var Deny = denyAuthenticator{}
+
+type denyAuthenticator struct{}
+
+func (denyAuthenticator) AuthenticatePassword(user, password string) (api.UserInfo, bool, error) {
+	return nil, false, nil
+}
+
+func (denyAuthenticator) AuthenticateAssertion(assertionType, data string) (api.UserInfo, bool, error) {
+	return nil, false, nil
+}
+
+func (denyAuthenticator) AuthenticateClient(client api.Client) (api.UserInfo, bool, error) {
+	return nil, false, nil
+}

pkg/authn/oauth/handlers/grant.go
@@ -1,0 +1,50 @@
+package handlers
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/RangelReale/osin"
+
+	"github.com/openshift/origin/pkg/authn/api"
+)
+
+type GrantCheck struct {
+	check   GrantChecker
+	handler GrantHandler
+}
+
+func NewGrantCheck(check GrantChecker, handler GrantHandler) *GrantCheck {
+	return &GrantCheck{check, handler}
+}
+
+func (h *GrantCheck) HandleAuthorize(ar *osin.AuthorizeRequest, w http.ResponseWriter, req *http.Request) (handled bool) {
+	if !ar.Authorized {
+		return
+	}
+
+	user, ok := ar.UserData.(api.UserInfo)
+	if !ok || user == nil {
+		h.handler.GrantError(errors.New("the provided user data is not api.UserInfo"), w, req)
+		return true
+	}
+
+	grant := &api.Grant{
+		Client:      ar.Client,
+		Scope:       ar.Scope,
+		Expiration:  int64(ar.Expiration),
+		RedirectURI: ar.RedirectUri,
+	}
+
+	ok, err := h.check.HasAuthorizedClient(ar.Client, user, grant)
+	if err != nil {
+		h.handler.GrantError(err, w, req)
+		return true
+	}
+	if !ok {
+		h.handler.GrantNeeded(grant, w, req)
+		return true
+	}
+
+	return
+}

pkg/authn/oauth/registry/grantchecker.go
@@ -1,0 +1,38 @@
+package registry
+
+import (
+	"fmt"
+
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/api/errors"
+
+	"github.com/openshift/origin/pkg/authn/api"
+	"github.com/openshift/origin/pkg/oauth/registry/clientauthorization"
+	"github.com/openshift/origin/pkg/oauth/scope"
+)
+
+type ClientAuthorizationGrantChecker struct {
+	registry clientauthorization.Registry
+}
+
+func NewClientAuthorizationGrantChecker(registry clientauthorization.Registry) *ClientAuthorizationGrantChecker {
+	return &ClientAuthorizationGrantChecker{registry}
+}
+
+func (c *ClientAuthorizationGrantChecker) HasAuthorizedClient(client api.Client, user api.UserInfo, grant *api.Grant) (bool, error) {
+	id := c.registry.ClientAuthorizationID(user.GetName(), client.GetId())
+	authorization, err := c.registry.GetClientAuthorization(id)
+	if errors.IsNotFound(err) {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	if authorization.UserUID != user.GetUID() {
+		return false, fmt.Errorf("user %s UID %s does not match stored client authorization value for UID %s", user.GetName(), user.GetUID(), authorization.UserUID)
+	}
+	// TODO: improve this to allow the scope implementation to determine overlap
+	if !scope.Covers(authorization.Scopes, scope.Split(grant.Scope)) {
+		return false, nil
+	}
+	return true, nil
+}

pkg/authn/server/login/login.go
@@ -1,0 +1,135 @@
+package login
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/golang/glog"
+
+	"github.com/openshift/origin/pkg/authn/api"
+	"github.com/openshift/origin/pkg/authn/authenticator"
+)
+
+type PasswordAuthenticator interface {
+	authenticator.Password
+	AuthenticationSucceeded(context api.UserInfo, then string, w http.ResponseWriter, req *http.Request)
+}
+
+type LoginFormRenderer interface {
+	Render(form LoginForm, w http.ResponseWriter, req *http.Request)
+}
+
+type LoginForm struct {
+	Action string
+	Error  string
+	Values LoginFormValues
+}
+
+type LoginFormValues struct {
+	Then     string
+	CSRF     string
+	Username string
+	Password string
+}
+
+type Login struct {
+	csrf   CSRF
+	auth   PasswordAuthenticator
+	render LoginFormRenderer
+}
+
+func NewLogin(csrf CSRF, auth PasswordAuthenticator, render LoginFormRenderer) *Login {
+	return &Login{
+		csrf:   csrf,
+		auth:   auth,
+		render: render,
+	}
+}
+
+func (l *Login) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		l.handleLoginForm(w, req)
+	case "POST":
+		l.handleLogin(w, req)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func (l *Login) handleLoginForm(w http.ResponseWriter, req *http.Request) {
+	uri, err := getBaseURL(req)
+	if err != nil {
+		glog.Errorf("Unable to generate base URL: %v", err)
+		http.Error(w, "Unable to determine URL", http.StatusInternalServerError)
+		return
+	}
+
+	form := LoginForm{
+		Action: uri.String(),
+	}
+	if then := req.URL.Query().Get("then"); then != "" {
+		// TODO: sanitize 'then'
+		form.Values.Then = then
+	}
+	switch req.URL.Query().Get("reason") {
+	case "":
+		break
+	default:
+		form.Error = "An unknown error has occured. Please try again."
+	}
+
+	csrf, err := l.csrf.Generate()
+	if err != nil {
+		glog.Errorf("Unable to generate CSRF token: %v", err)
+	}
+	form.Values.CSRF = csrf
+
+	l.render.Render(form, w, req)
+}
+
+func (l *Login) handleLogin(w http.ResponseWriter, req *http.Request) {
+	if ok, err := l.csrf.Check(req.FormValue("csrf")); !ok || err != nil {
+		glog.Errorf("Unable to check CSRF token: %v", err)
+		failed("token expired", w, req)
+		return
+	}
+	then := req.FormValue("then")
+	user, password := req.FormValue("username"), req.FormValue("password")
+	if user == "" {
+		failed("user required", w, req)
+		return
+	}
+	context, ok, err := l.auth.AuthenticatePassword(user, password)
+	if err != nil {
+		glog.Errorf("Unable to authenticate password: %v", err)
+		failed("unknown error", w, req)
+		return
+	}
+	if !ok {
+		failed("access denied", w, req)
+		return
+	}
+	l.auth.AuthenticationSucceeded(context, then, w, req)
+}
+
+var DefaultLoginFormRenderer = loginTemplateRenderer{}
+
+type loginTemplateRenderer struct{}
+
+func (r loginTemplateRenderer) Render(form LoginForm, w http.ResponseWriter, req *http.Request) {
+	if err := loginTemplate.Execute(w, form); err != nil {
+		glog.Errorf("Unable to render login template: %v", err)
+	}
+}
+
+var loginTemplate = template.Must(template.New("loginForm").Parse(`
+{{ if .Error }}<div class="message">{{ .Error }}</div>{{ end }}
+<form action="{{ .Action }}" method="POST">
+  <input type="hidden" name="then" value="{{ .Values.Then }}">
+  <input type="hidden" name="csrf" value="{{ .Values.CSRF }}">
+  <label>Login: <input type="text" name="username" value="{{ .Values.Username }}"></label>
+  <label>Password: <input type="password" name="password" value=""></label>
+  <input type="submit" name="Login">
+</form>
+`))

pkg/authn/server/login/login_test.go
@@ -1,0 +1,191 @@
+package login
+
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/openshift/origin/pkg/authn/api"
+)
+
+type testCSRF struct {
+	Token string
+	Err   error
+}
+
+func (t *testCSRF) Generate() (string, error) {
+	return t.Token, t.Err
+}
+
+func (t *testCSRF) Check(token string) (bool, error) {
+	return t.Token == token, t.Err
+}
+
+type testAuth struct {
+	Username string
+	Password string
+	User     api.UserInfo
+	Success  bool
+	Err      error
+	Then     string
+	Called   bool
+}
+
+func (t *testAuth) AuthenticatePassword(user, password string) (api.UserInfo, bool, error) {
+	t.Username = user
+	t.Password = password
+	return t.User, t.Success, t.Err
+}
+
+func (t *testAuth) AuthenticationSucceeded(user api.UserInfo, then string, w http.ResponseWriter, req *http.Request) {
+	t.Called = true
+	t.User = user
+	t.Then = then
+}
+
+func TestLogin(t *testing.T) {
+	testCases := map[string]struct {
+		CSRF       *testCSRF
+		Auth       *testAuth
+		Path       string
+		PostValues url.Values
+
+		ExpectStatusCode int
+		ExpectRedirect   string
+		ExpectContains   []string
+		ExpectThen       string
+	}{
+		"display form": {
+			CSRF: &testCSRF{Token: "test"},
+			Auth: &testAuth{},
+			Path: "/login",
+
+			ExpectStatusCode: 200,
+			ExpectContains: []string{
+				`action="/login"`,
+				`name="csrf" value="test"`,
+			},
+		},
+		"display form with errors": {
+			CSRF: &testCSRF{Token: "test"},
+			Auth: &testAuth{},
+			Path: "?then=foo&reason=failed&username=user",
+
+			ExpectStatusCode: 200,
+			ExpectContains: []string{
+				`action="/"`,
+				`name="then" value="foo"`,
+				`"message">An unknown error has occured`,
+			},
+		},
+		"redirect when POST fails CSRF": {
+			CSRF:           &testCSRF{Token: "test"},
+			Auth:           &testAuth{},
+			Path:           "/login",
+			PostValues:     url.Values{"csrf": []string{"wrong"}},
+			ExpectRedirect: "/login?reason=token+expired",
+		},
+		"redirect with 'then' when POST fails CSRF": {
+			CSRF:           &testCSRF{Token: "test"},
+			Auth:           &testAuth{},
+			Path:           "/login?then=test",
+			PostValues:     url.Values{"csrf": []string{"wrong"}},
+			ExpectRedirect: "/login?reason=token+expired&then=test",
+		},
+		"redirect when no username": {
+			CSRF: &testCSRF{Token: "test"},
+			Auth: &testAuth{},
+			Path: "/login",
+			PostValues: url.Values{
+				"csrf": []string{"test"},
+			},
+			ExpectRedirect: "/login?reason=user+required",
+		},
+		"redirect when not authenticated": {
+			CSRF: &testCSRF{Token: "test"},
+			Auth: &testAuth{Success: false},
+			Path: "/login",
+			PostValues: url.Values{
+				"csrf":     []string{"test"},
+				"username": []string{"user"},
+			},
+			ExpectRedirect: "/login?reason=access+denied",
+		},
+		"redirect on auth error": {
+			CSRF: &testCSRF{Token: "test"},
+			Auth: &testAuth{Err: errors.New("failed")},
+			Path: "/login",
+			PostValues: url.Values{
+				"csrf":     []string{"test"},
+				"username": []string{"user"},
+			},
+			ExpectRedirect: "/login?reason=unknown+error",
+		},
+		"login successful": {
+			CSRF: &testCSRF{Token: "test"},
+			Auth: &testAuth{Success: true, User: &api.DefaultUserInfo{Name: "user"}},
+			Path: "/login?then=done",
+			PostValues: url.Values{
+				"csrf":     []string{"test"},
+				"username": []string{"user"},
+			},
+			ExpectThen: "done",
+		},
+	}
+
+	for k, testCase := range testCases {
+		server := httptest.NewServer(NewLogin(testCase.CSRF, testCase.Auth, DefaultLoginFormRenderer))
+
+		var resp *http.Response
+		if testCase.PostValues != nil {
+			r, err := http.PostForm(server.URL+testCase.Path, testCase.PostValues)
+			if err != nil {
+				t.Errorf("%s: unexpected error: %v", k, err)
+				continue
+			}
+			resp = r
+		} else {
+			r, err := http.Get(server.URL + testCase.Path)
+			if err != nil {
+				t.Errorf("%s: unexpected error: %v", k, err)
+				continue
+			}
+			resp = r
+		}
+
+		if testCase.ExpectStatusCode != 0 && testCase.ExpectStatusCode != resp.StatusCode {
+			t.Errorf("%s: unexpected response: %#v", k, resp)
+			continue
+		}
+
+		if testCase.ExpectRedirect != "" {
+			uri, err := resp.Location()
+			if err != nil {
+				t.Errorf("%s: unexpected error: %v", k, err)
+				continue
+			}
+			if uri.String() != server.URL+testCase.ExpectRedirect {
+				t.Errorf("%s: unexpected redirect: %s", k, uri.String())
+			}
+		}
+
+		if testCase.ExpectThen != "" && (!testCase.Auth.Called || testCase.Auth.Then != testCase.ExpectThen) {
+			t.Errorf("%s: did not find expected 'then' value: %#v", k, testCase.Auth)
+		}
+
+		if len(testCase.ExpectContains) > 0 {
+			data, _ := ioutil.ReadAll(resp.Body)
+			body := string(data)
+			for i := range testCase.ExpectContains {
+				if !strings.Contains(body, testCase.ExpectContains[i]) {
+					t.Errorf("%s: did not find expected value %s: %s", k, testCase.ExpectContains[i], body)
+					continue
+				}
+			}
+		}
+	}
+}

openshift/oauth-server

pkg/auth/authenticator/requestheader/requestheader.go
@@ -1,0 +1,36 @@
+package requestheader
+
+import (
+	"net/http"
+
+	"github.com/openshift/origin/pkg/auth/api"
+)
+
+type Config struct {
+	UserNameHeader string
+}
+
+func NewDefaultConfig() *Config {
+	return &Config{
+		UserNameHeader: "X-Remote-User",
+	}
+}
+
+type Authenticator struct {
+	config *Config
+}
+
+func NewAuthenticator(config *Config) *Authenticator {
+	return &Authenticator{config}
+}
+
+func (a *Authenticator) AuthenticateRequest(req *http.Request) (api.UserInfo, bool, error) {
+	name := req.Header.Get(a.config.UserNameHeader)
+	if name == "" {
+		return nil, false, nil
+	}
+	user := &api.DefaultUserInfo{
+		Name: name,
+	}
+	return user, true, nil
+}

pkg/auth/oauth/handlers/authenticator.go
@@ -1,0 +1,108 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/RangelReale/osin"
+	"github.com/golang/glog"
+
+	"github.com/openshift/origin/pkg/auth/api"
+	"github.com/openshift/origin/pkg/auth/authenticator"
+)
+
+type AuthorizeAuthenticator struct {
+	handler AuthenticationHandler
+	request authenticator.Request
+}
+
+func NewAuthorizeAuthenticator(handler AuthenticationHandler, request authenticator.Request) *AuthorizeAuthenticator {
+	return &AuthorizeAuthenticator{handler, request}
+}
+
+func (h *AuthorizeAuthenticator) HandleAuthorize(ar *osin.AuthorizeRequest, w http.ResponseWriter, req *http.Request) (handled bool) {
+	info, ok, err := h.request.AuthenticateRequest(req)
+	if err != nil {
+		h.handler.AuthenticationError(err, w, req)
+		return true
+	}
+	if !ok {
+		h.handler.AuthenticationNeeded(w, req)
+		return true
+	}
+	ar.UserData = info
+	ar.Authorized = true
+	return
+}
+
+type AccessAuthenticator struct {
+	password  authenticator.Password
+	assertion authenticator.Assertion
+	client    authenticator.Client
+}
+
+func NewAccessAuthenticator(password authenticator.Password, assertion authenticator.Assertion, client authenticator.Client) *AccessAuthenticator {
+	return &AccessAuthenticator{password, assertion, client}
+}
+
+func (h *AccessAuthenticator) HandleAccess(ar *osin.AccessRequest, w http.ResponseWriter, req *http.Request) {
+	switch ar.Type {
+	case osin.AUTHORIZATION_CODE, osin.REFRESH_TOKEN:
+		// auth codes and refresh tokens are assumed allowed
+	case osin.PASSWORD:
+		info, ok, err := h.password.AuthenticatePassword(ar.Username, ar.Password)
+		if err != nil {
+			glog.Errorf("Unable to authenticate password: %v", err)
+			return
+		}
+		if !ok {
+			return
+		}
+		ar.AccessData.UserData = info
+	case osin.ASSERTION:
+		info, ok, err := h.assertion.AuthenticateAssertion(ar.AssertionType, ar.Assertion)
+		if err != nil {
+			glog.Errorf("Unable to authenticate password: %v", err)
+			return
+		}
+		if !ok {
+			return
+		}
+		ar.AccessData.UserData = info
+	case osin.CLIENT_CREDENTIALS:
+		info, ok, err := h.client.AuthenticateClient(ar.Client)
+		if err != nil {
+			glog.Errorf("Unable to authenticate password: %v", err)
+			return
+		}
+		if !ok {
+			return
+		}
+		ar.AccessData.UserData = info
+		return
+	default:
+		glog.Warningf("Received unknown access token type: %s", ar.Type)
+		return
+	}
+
+	ar.Authorized = true
+}
+
+func NewDenyAccessAuthenticator() *AccessAuthenticator {
+	return &AccessAuthenticator{Deny, Deny, Deny}
+}
+
+var Deny = denyAuthenticator{}
+
+type denyAuthenticator struct{}
+
+func (denyAuthenticator) AuthenticatePassword(user, password string) (api.UserInfo, bool, error) {
+	return nil, false, nil
+}
+
+func (denyAuthenticator) AuthenticateAssertion(assertionType, data string) (api.UserInfo, bool, error) {
+	return nil, false, nil
+}
+
+func (denyAuthenticator) AuthenticateClient(client api.Client) (api.UserInfo, bool, error) {
+	return nil, false, nil
+}

pkg/auth/oauth/handlers/grant.go
@@ -1,0 +1,50 @@
+package handlers
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/RangelReale/osin"
+
+	"github.com/openshift/origin/pkg/auth/api"
+)
+
+type GrantCheck struct {
+	check   GrantChecker
+	handler GrantHandler
+}
+
+func NewGrantCheck(check GrantChecker, handler GrantHandler) *GrantCheck {
+	return &GrantCheck{check, handler}
+}
+
+func (h *GrantCheck) HandleAuthorize(ar *osin.AuthorizeRequest, w http.ResponseWriter, req *http.Request) (handled bool) {
+	if !ar.Authorized {
+		return
+	}
+
+	user, ok := ar.UserData.(api.UserInfo)
+	if !ok || user == nil {
+		h.handler.GrantError(errors.New("the provided user data is not api.UserInfo"), w, req)
+		return true
+	}
+
+	grant := &api.Grant{
+		Client:      ar.Client,
+		Scope:       ar.Scope,
+		Expiration:  int64(ar.Expiration),
+		RedirectURI: ar.RedirectUri,
+	}
+
+	ok, err := h.check.HasAuthorizedClient(ar.Client, user, grant)
+	if err != nil {
+		h.handler.GrantError(err, w, req)
+		return true
+	}
+	if !ok {
+		h.handler.GrantNeeded(grant, w, req)
+		return true
+	}
+
+	return
+}

pkg/auth/oauth/registry/grantchecker.go
@@ -1,0 +1,38 @@
+package registry
+
+import (
+	"fmt"
+
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/api/errors"
+
+	"github.com/openshift/origin/pkg/auth/api"
+	"github.com/openshift/origin/pkg/oauth/registry/clientauthorization"
+	"github.com/openshift/origin/pkg/oauth/scope"
+)
+
+type ClientAuthorizationGrantChecker struct {
+	registry clientauthorization.Registry
+}
+
+func NewClientAuthorizationGrantChecker(registry clientauthorization.Registry) *ClientAuthorizationGrantChecker {
+	return &ClientAuthorizationGrantChecker{registry}
+}
+
+func (c *ClientAuthorizationGrantChecker) HasAuthorizedClient(client api.Client, user api.UserInfo, grant *api.Grant) (bool, error) {
+	id := c.registry.ClientAuthorizationID(user.GetName(), client.GetId())
+	authorization, err := c.registry.GetClientAuthorization(id)
+	if errors.IsNotFound(err) {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	if len(authorization.UserUID) != 0 && authorization.UserUID != user.GetUID() {
+		return false, fmt.Errorf("user %s UID %s does not match stored client authorization value for UID %s", user.GetName(), user.GetUID(), authorization.UserUID)
+	}
+	// TODO: improve this to allow the scope implementation to determine overlap
+	if !scope.Covers(authorization.Scopes, scope.Split(grant.Scope)) {
+		return false, nil
+	}
+	return true, nil
+}

pkg/auth/server/login/login.go
@@ -1,0 +1,135 @@
+package login
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/golang/glog"
+
+	"github.com/openshift/origin/pkg/auth/api"
+	"github.com/openshift/origin/pkg/auth/authenticator"
+)
+
+type PasswordAuthenticator interface {
+	authenticator.Password
+	AuthenticationSucceeded(context api.UserInfo, then string, w http.ResponseWriter, req *http.Request)
+}
+
+type LoginFormRenderer interface {
+	Render(form LoginForm, w http.ResponseWriter, req *http.Request)
+}
+
+type LoginForm struct {
+	Action string
+	Error  string
+	Values LoginFormValues
+}
+
+type LoginFormValues struct {
+	Then     string
+	CSRF     string
+	Username string
+	Password string
+}
+
+type Login struct {
+	csrf   CSRF
+	auth   PasswordAuthenticator
+	render LoginFormRenderer
+}
+
+func NewLogin(csrf CSRF, auth PasswordAuthenticator, render LoginFormRenderer) *Login {
+	return &Login{
+		csrf:   csrf,
+		auth:   auth,
+		render: render,
+	}
+}
+
+func (l *Login) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		l.handleLoginForm(w, req)
+	case "POST":
+		l.handleLogin(w, req)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func (l *Login) handleLoginForm(w http.ResponseWriter, req *http.Request) {
+	uri, err := getBaseURL(req)
+	if err != nil {
+		glog.Errorf("Unable to generate base URL: %v", err)
+		http.Error(w, "Unable to determine URL", http.StatusInternalServerError)
+		return
+	}
+
+	form := LoginForm{
+		Action: uri.String(),
+	}
+	if then := req.URL.Query().Get("then"); then != "" {
+		// TODO: sanitize 'then'
+		form.Values.Then = then
+	}
+	switch req.URL.Query().Get("reason") {
+	case "":
+		break
+	default:
+		form.Error = "An unknown error has occured. Please try again."
+	}
+
+	csrf, err := l.csrf.Generate()
+	if err != nil {
+		glog.Errorf("Unable to generate CSRF token: %v", err)
+	}
+	form.Values.CSRF = csrf
+
+	l.render.Render(form, w, req)
+}
+
+func (l *Login) handleLogin(w http.ResponseWriter, req *http.Request) {
+	if ok, err := l.csrf.Check(req.FormValue("csrf")); !ok || err != nil {
+		glog.Errorf("Unable to check CSRF token: %v", err)
+		failed("token expired", w, req)
+		return
+	}
+	then := req.FormValue("then")
+	user, password := req.FormValue("username"), req.FormValue("password")
+	if user == "" {
+		failed("user required", w, req)
+		return
+	}
+	context, ok, err := l.auth.AuthenticatePassword(user, password)
+	if err != nil {
+		glog.Errorf("Unable to authenticate password: %v", err)
+		failed("unknown error", w, req)
+		return
+	}
+	if !ok {
+		failed("access denied", w, req)
+		return
+	}
+	l.auth.AuthenticationSucceeded(context, then, w, req)
+}
+
+var DefaultLoginFormRenderer = loginTemplateRenderer{}
+
+type loginTemplateRenderer struct{}
+
+func (r loginTemplateRenderer) Render(form LoginForm, w http.ResponseWriter, req *http.Request) {
+	if err := loginTemplate.Execute(w, form); err != nil {
+		glog.Errorf("Unable to render login template: %v", err)
+	}
+}
+
+var loginTemplate = template.Must(template.New("loginForm").Parse(`
+{{ if .Error }}<div class="message">{{ .Error }}</div>{{ end }}
+<form action="{{ .Action }}" method="POST">
+  <input type="hidden" name="then" value="{{ .Values.Then }}">
+  <input type="hidden" name="csrf" value="{{ .Values.CSRF }}">
+  <label>Login: <input type="text" name="username" value="{{ .Values.Username }}"></label>
+  <label>Password: <input type="password" name="password" value=""></label>
+  <input type="submit" name="Login">
+</form>
+`))

pkg/auth/server/login/login_test.go
@@ -1,0 +1,191 @@
+package login
+
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/openshift/origin/pkg/auth/api"
+)
+
+type testCSRF struct {
+	Token string
+	Err   error
+}
+
+func (t *testCSRF) Generate() (string, error) {
+	return t.Token, t.Err
+}
+
+func (t *testCSRF) Check(token string) (bool, error) {
+	return t.Token == token, t.Err
+}
+
+type testAuth struct {
+	Username string
+	Password string
+	User     api.UserInfo
+	Success  bool
+	Err      error
+	Then     string
+	Called   bool
+}
+
+func (t *testAuth) AuthenticatePassword(user, password string) (api.UserInfo, bool, error) {
+	t.Username = user
+	t.Password = password
+	return t.User, t.Success, t.Err
+}
+
+func (t *testAuth) AuthenticationSucceeded(user api.UserInfo, then string, w http.ResponseWriter, req *http.Request) {
+	t.Called = true
+	t.User = user
+	t.Then = then
+}
+
+func TestLogin(t *testing.T) {
+	testCases := map[string]struct {
+		CSRF       *testCSRF
+		Auth       *testAuth
+		Path       string
+		PostValues url.Values
+
+		ExpectStatusCode int
+		ExpectRedirect   string
+		ExpectContains   []string
+		ExpectThen       string
+	}{
+		"display form": {
+			CSRF: &testCSRF{Token: "test"},
+			Auth: &testAuth{},
+			Path: "/login",
+
+			ExpectStatusCode: 200,
+			ExpectContains: []string{
+				`action="/login"`,
+				`name="csrf" value="test"`,
+			},
+		},
+		"display form with errors": {
+			CSRF: &testCSRF{Token: "test"},
+			Auth: &testAuth{},
+			Path: "?then=foo&reason=failed&username=user",
+
+			ExpectStatusCode: 200,
+			ExpectContains: []string{
+				`action="/"`,
+				`name="then" value="foo"`,
+				`"message">An unknown error has occured`,
+			},
+		},
+		"redirect when POST fails CSRF": {
+			CSRF:           &testCSRF{Token: "test"},
+			Auth:           &testAuth{},
+			Path:           "/login",
+			PostValues:     url.Values{"csrf": []string{"wrong"}},
+			ExpectRedirect: "/login?reason=token+expired",
+		},
+		"redirect with 'then' when POST fails CSRF": {
+			CSRF:           &testCSRF{Token: "test"},
+			Auth:           &testAuth{},
+			Path:           "/login?then=test",
+			PostValues:     url.Values{"csrf": []string{"wrong"}},
+			ExpectRedirect: "/login?reason=token+expired&then=test",
+		},
+		"redirect when no username": {
+			CSRF: &testCSRF{Token: "test"},
+			Auth: &testAuth{},
+			Path: "/login",
+			PostValues: url.Values{
+				"csrf": []string{"test"},
+			},
+			ExpectRedirect: "/login?reason=user+required",
+		},
+		"redirect when not authenticated": {
+			CSRF: &testCSRF{Token: "test"},
+			Auth: &testAuth{Success: false},
+			Path: "/login",
+			PostValues: url.Values{
+				"csrf":     []string{"test"},
+				"username": []string{"user"},
+			},
+			ExpectRedirect: "/login?reason=access+denied",
+		},
+		"redirect on auth error": {
+			CSRF: &testCSRF{Token: "test"},
+			Auth: &testAuth{Err: errors.New("failed")},
+			Path: "/login",
+			PostValues: url.Values{
+				"csrf":     []string{"test"},
+				"username": []string{"user"},
+			},
+			ExpectRedirect: "/login?reason=unknown+error",
+		},
+		"login successful": {
+			CSRF: &testCSRF{Token: "test"},
+			Auth: &testAuth{Success: true, User: &api.DefaultUserInfo{Name: "user"}},
+			Path: "/login?then=done",
+			PostValues: url.Values{
+				"csrf":     []string{"test"},
+				"username": []string{"user"},
+			},
+			ExpectThen: "done",
+		},
+	}
+
+	for k, testCase := range testCases {
+		server := httptest.NewServer(NewLogin(testCase.CSRF, testCase.Auth, DefaultLoginFormRenderer))
+
+		var resp *http.Response
+		if testCase.PostValues != nil {
+			r, err := http.PostForm(server.URL+testCase.Path, testCase.PostValues)
+			if err != nil {
+				t.Errorf("%s: unexpected error: %v", k, err)
+				continue
+			}
+			resp = r
+		} else {
+			r, err := http.Get(server.URL + testCase.Path)
+			if err != nil {
+				t.Errorf("%s: unexpected error: %v", k, err)
+				continue
+			}
+			resp = r
+		}
+
+		if testCase.ExpectStatusCode != 0 && testCase.ExpectStatusCode != resp.StatusCode {
+			t.Errorf("%s: unexpected response: %#v", k, resp)
+			continue
+		}
+
+		if testCase.ExpectRedirect != "" {
+			uri, err := resp.Location()
+			if err != nil {
+				t.Errorf("%s: unexpected error: %v", k, err)
+				continue
+			}
+			if uri.String() != server.URL+testCase.ExpectRedirect {
+				t.Errorf("%s: unexpected redirect: %s", k, uri.String())
+			}
+		}
+
+		if testCase.ExpectThen != "" && (!testCase.Auth.Called || testCase.Auth.Then != testCase.ExpectThen) {
+			t.Errorf("%s: did not find expected 'then' value: %#v", k, testCase.Auth)
+		}
+
+		if len(testCase.ExpectContains) > 0 {
+			data, _ := ioutil.ReadAll(resp.Body)
+			body := string(data)
+			for i := range testCase.ExpectContains {
+				if !strings.Contains(body, testCase.ExpectContains[i]) {
+					t.Errorf("%s: did not find expected value %s: %s", k, testCase.ExpectContains[i], body)
+					continue
+				}
+			}
+		}
+	}
+}