7fcd75741fdafff5770a0a28e7f85926b6c34804
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2021-12-22 13:35:12 +0100

    pkg/*: introduce x509_missing_san_total metric

--- pkg/prometheus/metrics.go
+++ pkg/prometheus/metrics.go
@@ -1,6 +1,8 @@
 package metrics
 
 import (
+	configv1 "github.com/openshift/api/config/v1"
+
 	"k8s.io/component-base/metrics"
 	"k8s.io/component-base/metrics/legacyregistry"
 )
@@ -51,6 +53,16 @@ var (
 			Help:      "Counts basic password authentication attempts by result",
 		}, []string{"result"},
 	)
+	X509MissingSANCounter = metrics.NewCounterVec(
+		&metrics.CounterOpts{
+			Subsystem: authSubsystem,
+			Name:      "x509_missing_san_total",
+			Help: "Counts the number of requests to servers missing SAN extension " +
+				"in their serving certificate OR the number of connection failures " +
+				"due to the lack of x509 certificate SAN extension missing " +
+				"(either/or, based on the runtime environment)",
+		}, []string{"provider"},
+	)
 )
 
 func init() {
@@ -59,10 +71,23 @@ func init() {
 	legacyregistry.MustRegister(authFormCounterResult)
 	legacyregistry.MustRegister(authBasicCounter)
 	legacyregistry.MustRegister(authBasicCounterResult)
+	legacyregistry.MustRegister(X509MissingSANCounter)
 
 	for _, resultLabel := range []string{SuccessResult, FailResult, ErrorResult} {
 		authBasicCounterResult.WithLabelValues(resultLabel)
 		authFormCounterResult.WithLabelValues(resultLabel)
+	}
+
+	for _, provider := range [...]configv1.IdentityProviderType{
+		configv1.IdentityProviderTypeBasicAuth,
+		configv1.IdentityProviderTypeGitHub,
+		configv1.IdentityProviderTypeGitLab,
+		configv1.IdentityProviderTypeGoogle,
+		configv1.IdentityProviderTypeKeystone,
+		configv1.IdentityProviderTypeLDAP,
+		configv1.IdentityProviderTypeOpenID,
+	} {
+		X509MissingSANCounter.WithLabelValues(string(provider))
 	}
 }
 
